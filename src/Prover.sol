c7a1758c4f8b39266d5dc172b9c69276aab859c4
sonicskye <sonic_skye@yahoo.com>
2024-05-08 11:58:13 +1000

    Prover skeleton

---
+++ src/Prover.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {MerkleProof} from '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
+
+contract Prover {
+
+    function proof() public {
+        
+    }
+}
