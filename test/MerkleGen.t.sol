23fcbfabad6b7336e6f4711b5814d6a3deaf4265
sonicskye <sonic_skye@yahoo.com>
2024-05-22 05:05:00 +1000

    testing

--- test/MerkleGen.t.sol
+++ test/MerkleGen.t.sol
@@ -16,6 +16,7 @@ contract MerkleGenTest is Test {
         prover = new Prover();
     }
 
+    /// @notice A standard 4-leaf Merkle tree with all known values
     function test_prove() public {
         // Generate an array of bytes32 leaves
         bytes32[] memory leaves = new bytes32[](4);
@@ -54,9 +55,11 @@ contract MerkleGenTest is Test {
         emit log_named_bytes32("h0123", h0123);
 
         // Generate selected indexes
-        uint256[] memory indices = new uint256[](2);
+        uint256[] memory indices = new uint256[](4);
         indices[0] = 0;
         indices[1] = 1;
+        indices[2] = 2;
+        indices[3] = 3;
 
         bytes32[] memory leaf_indexes = new bytes32[](indices.length);
         for (uint256 i = 0; i < indices.length; i++) {
@@ -71,6 +74,56 @@ contract MerkleGenTest is Test {
         // Verify the proof
         assertTrue(prover.prove(proof, proofFlagBits, root, leaf_indexes));
         
+    }
+
+    /// @notice A fuzz test for the Merkle tree
+    function testFuzz_prove(uint256 seed, bool[] memory select_leaves_, uint256 numLeaves) public {
+        //uint256 numLeaves = 5;
+        // Assume
+        numLeaves = bound(numLeaves, 1, 10000);
+        vm.assume(select_leaves_.length >= numLeaves);
+        // Seed for generating leaves
+        seed = bound(seed, 1 ether, 1000 ether);
+
+        /*select_leaves_ = new bool[](numLeaves);
+        select_leaves_[0] = true;
+        select_leaves_[1] = true;
+        select_leaves_[2] = true;
+        select_leaves_[3] = true;*/
+
+        // Count the number of selected leaves
+        uint256 numIndexes = 0;
+        for (uint256 i = 0; i < numLeaves; i++) {
+            if (select_leaves_[i]) {
+                numIndexes += 1;
+            }
+        }
+
+        vm.assume(numIndexes > 0 && numIndexes <= numLeaves);
+
+        // Generate an array of bytes32 leaves
+        bytes32[] memory leaves = new bytes32[](numLeaves);
+        for (uint256 i = 0; i < numLeaves; i++) {
+            leaves[i] = keccak256(abi.encodePacked(seed + i));
+        }
+
+        // Generate selected indexes
+        uint256[] memory indices = new uint256[](numIndexes);
+        bytes32[] memory leaf_indexes = new bytes32[](numIndexes);
+        uint256 j = 0;
+        for (uint256 i = 0; i < numLeaves; i++) {
+            if (select_leaves_[i]) {
+                indices[j] = i;
+                leaf_indexes[j] = leaves[i];
+                j += 1;
+            }
+        }
+
+        // Generate the proof
+        (bytes32[] memory proof, bool[] memory proofFlagBits, bytes32 root) = merkleGen.gen(leaves, indices);
+
+        // Verify the proof
+        assertTrue(prover.prove(proof, proofFlagBits, root, leaf_indexes));
     }
 
 
